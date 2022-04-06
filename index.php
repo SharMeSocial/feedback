3952afdd5e11277c2dea9eb3d9ce1734e59ba85c
kaplego <70806521+kaplego@users.noreply.github.com>
2022-04-06 16:29:35 +0200

    Source code available sonn -> here

--- index.php
+++ index.php
@@ -266,7 +266,7 @@
                     <button class="btn btn-primary w-100" type="submit" id="submitbt">Submit</button>
                 </div>
             </form>
-            <div class="position-absolute bottom-0 mb-2 text-primary" style="z-index:501;user-select: none;font-weight: bold;">Source code available soon !</div>
+            <div class="position-fixed bottom-0 mb-2 text-primary" style="z-index:501;user-select: none;font-weight: bold;">Source code available <a target="_blank" href="https://github.com/SharMeSocial/feedback">here</a></div>
         </main>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
